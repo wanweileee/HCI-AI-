c4ce0b5fa0cba1249346041237145199b27945bf
fawziyah01 <1007214@mymail.sutd.edu.sg>
2024-07-01 21:26:03 +0800

    added all routes and included navbar

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,29 @@ from flask import Blueprint, render_template
 main = Blueprint('main', __name__)
 
 @main.route('/')
-def index():
+def home():
     return render_template('homepage.html')
 
+@main.route('/goals')
+def goals():
+    return render_template('goals.html')
 
+@main.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+@main.route('/add')
+def add():
+    return render_template('add.html')
+
+@main.route('/scan')
+def scan():
+    return render_template('scan.html')
+
+@main.route('/login')
+def login():
+    return render_template('login.html')
+
+@main.route('/signup')
+def signup():
+    return render_template('signup.html')
